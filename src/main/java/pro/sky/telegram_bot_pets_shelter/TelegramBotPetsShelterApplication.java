21262bca7811ccce4f39df9b8e9b505315b79b3c
igor-gunin75 <igor-gunin75@yandex.ru>
2023-03-13 17:19:27 +0600

    corrected pom

--- src/main/java/pro/sky/telegram_bot_pets_shelter/TelegramBotPetsShelterApplication.java
+++ src/main/java/pro/sky/telegram_bot_pets_shelter/TelegramBotPetsShelterApplication.java
@@ -2,10 +2,12 @@ package pro.sky.telegram_bot_pets_shelter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 public class TelegramBotPetsShelterApplication {
 
     public static void main(String[] args) {
